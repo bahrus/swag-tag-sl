4f4191c0cf50859e56fcb7490e8d165f4892feee
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-08-30 14:13:18 -0400

    ...

--- swag-tag-catalog-sl.js
+++ swag-tag-catalog-sl.js
@@ -1,12 +1,14 @@
 import { XtalElement, define } from 'xtal-element/XtalElement.js';
 import { createTemplate } from 'trans-render/createTemplate.js';
 import('p-et-alia/p-d.js');
+import('p-et-alia/p-u.js');
 const mainTemplate = createTemplate(/* html */ `
 <sl-icon-button name=card-list data-click-value="true"></sl-icon-button>
 <p-d on=click to=[-open] m=1 val=target.dataset.clickValue parse-val-as=boolean skip-init></p-d>
 <sl-drawer label="Drawer" -open class="drawer-overview">
   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-  <sl-button slot="footer" type="primary">Close</sl-button>
+  <sl-button slot="footer" type="primary" data-click-value="false">Close</sl-button>
+  <p-u on=click to-closest=sl-drawer prop=open val=target.dataset.clickValue parse-val-as=boolean skip-init></p-u>
 </sl-drawer>
 
 
