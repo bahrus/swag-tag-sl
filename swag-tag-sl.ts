4ddcfe05b289391aa5fbf7b8ddba70a587e16660
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-07-25 20:22:24 -0400

    ...

--- swag-tag-sl.ts
+++ swag-tag-sl.ts
@@ -1,80 +1,12 @@
-import {
-    SwagTag, uiRefs, bindName, addEventListeners, linkWcInfo, triggerImportReferencedModule, 
-    adjustValueAndType, bindSelf, showHideEditor, linkInnerTemplate, copyPropInfoIntoEditor} from 'swag-tag/swag-tag.js';
-  import {define} from 'xtal-element/XtalElement.js';
-  import {RenderContext, PEATSettings} from 'trans-render/types.d.js';
-  import {SwagTagSLTextField} from './lib/swag-tag-sl-textfield.js';
-  import {SwagTagSLCheckbox} from './lib/swag-tag-sl-checkbox.js';
-  import {SwagTagJsonEditor} from 'swag-tag/lib/swag-tag-json-editor.js';
-  import {SwagTagSLSelect} from './lib/swag-tag-sl-select.js';
-  import { SelectiveUpdate} from "../xtal-element/types.js";
+import {SwagTagBase, xc, xp, html} from 'swag-tag/lib/swag-tag-base.js';
+import('@shoelace-style/shoelace/dist/components/button/button.js');
 
-  
-  const copyPropInfoIntoEditors = {
-    [`${SwagTagSLTextField.is},${SwagTagSLCheckbox.is},${SwagTagJsonEditor.is},${SwagTagSLSelect.is}`]: copyPropInfoIntoEditor,
-  };
-  
-  export const addEditors =   ({massagedProps, name}: SwagTag) => ({
-      // Loop over massagedProps, and insert dynamic editor via tag name (item.editor is the tag name)
-      [uiRefs.scrollableArea]: [
-        //Array to loop over
-        massagedProps, 
-        //A **toTagOrTemplate** function that returns a string -- used to generate a (custom element) with the name of the string.
-        ({item}: RenderContext) => (<any>item).editor,
-        //empty range
-        ,
-        //now that document.createElement(tag) done, apply transform
-        copyPropInfoIntoEditors
-      ]
-  });
-  
-  const massaged = Symbol();
-  export const linkMassagedProps = ({properties, self, block}: SwagTag) => {
-      
-      if(properties === undefined || (<any>properties)[massaged as any as string]) return;
-      properties.forEach(prop =>{
-        adjustValueAndType(prop);
-        const anyProp = <any>prop;
-        let defaultVal = anyProp.default;
-        switch(prop.type){
-          case 'string':
-          case 'number':
-            anyProp.editor = SwagTagSLTextField.is;
-            break;
-          case 'boolean':
-            anyProp.editor = SwagTagSLCheckbox.is;
-            break;
-          case 'object':
-            anyProp.editor = SwagTagJsonEditor.is;
-            break;
-          case 'stringArray':
-            anyProp.editor = SwagTagSLSelect.is;
-            break;
-          default:
-            throw 'Not implemented';
-            
-        }
-      });
-      (<any>properties)[massaged as any as string] = true;
-      self.massagedProps = block !== undefined ? properties.filter(prop => !block.includes(prop.name!)) : properties;
-    }
-  
-  const updateTransforms = [
-      bindName,
-      addEventListeners,
-      addEditors,
-      bindSelf,
-    ] as SelectiveUpdate<any>[];
-  
-  export class SwagTagSL extends SwagTag{
-      static is = 'swag-tag-sl';
-  
-      updateTransforms = updateTransforms;
-  
-      propActions = [
-          linkWcInfo, linkMassagedProps, triggerImportReferencedModule, showHideEditor, linkInnerTemplate
-      ];
-  
-  }
-  
-  define(SwagTagSL);
+//import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
+export const editor = html `
+<sl-button type="primary">Primary</sl-button>
+`;
+export class SwagTagSL extends SwagTagBase{
+  static is = 'swag-tag-sl';
+  editor = editor;
+}
+xc.define(SwagTagSL);
