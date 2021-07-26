242bf81239bdb2ccfeeae64548510993d73d1c80
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-07-25 22:54:50 -0400

    ...

--- swag-tag-sl.ts
+++ swag-tag-sl.ts
@@ -1,8 +1,13 @@
 import {SwagTagBase, xc, xp, html} from 'swag-tag/lib/swag-tag-base.js';
+import('lib-id/li-bid.js');
 import('@shoelace-style/shoelace/dist/components/button/button.js');
+import('carbon-copy/c-c.js');
+import('pass-prop/p-p.js');
 
 //import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
 export const editor = html `
+<p-p from-host observe-prop=fields to=[-list] m=1></p-p>
+<li-bid -list><sl-button></sl-button></li-bid>
 <sl-button type="primary">Primary</sl-button>
 `;
 export class SwagTagSL extends SwagTagBase{
