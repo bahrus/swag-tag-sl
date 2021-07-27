7c9c0eac887ecbf55b04c8f880fb9818024b98a9
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-07-26 22:53:06 -0400

    ...

--- swag-tag-sl.ts
+++ swag-tag-sl.ts
@@ -3,16 +3,18 @@ import('lib-id/li-bid.js');
 import('@shoelace-style/shoelace/dist/components/button/button.js');
 import('carbon-copy/c-c.js');
 import('pass-prop/p-p.js');
+import('if-diff/if-diff.js');
+
 
 //import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
 export const editor = html `
+<p-d observe=wc-info-fetch vft=fields to=[-list] m=1></p-d>
+<i-bid -list><stsl-field-editor></stsl-field-editor></i-bid>
+
 <template id=stsl-field-editor>
 <sl-button type="primary">Primary</sl-button>
 </template>
 <c-c copy from-prev-sibling></c-c>
-<p-p from-host observe-prop=fields to=[-list] m=1></p-p>
-<i-bid -list><stsl-field-editor></stsl-field-editor></i-bid>
-
 `;
 export class SwagTagSL extends SwagTagBase{
   static is = 'swag-tag-sl';
