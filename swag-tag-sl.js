b9e8bf5ee6cd45ac7fec75bf91780d5e38ffb0c3
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-07-27 07:32:46 -0400

    ...

--- swag-tag-sl.js
+++ swag-tag-sl.js
@@ -1,16 +1,18 @@
 import { SwagTagBase, xc, html } from 'swag-tag/lib/swag-tag-base.js';
-import('lib-id/li-bid.js');
+import { IBid } from 'ib-id/i-bid.js';
 import('@shoelace-style/shoelace/dist/components/button/button.js');
+import('@shoelace-style/shoelace/dist/components/input/input.js');
+import('@shoelace-style/shoelace/dist/components/textarea/textarea.js');
 import('carbon-copy/c-c.js');
 import('pass-prop/p-p.js');
 import('if-diff/if-diff.js');
 //import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
 export const editor = html `
 <p-d observe=wc-info-fetch vft=fields to=[-list] m=1></p-d>
-<i-bid -list><stsl-field-editor></stsl-field-editor></i-bid>
+<swag-tag-sl-i-bid -list><stsl-general-field-editor></stsl-general-field-editor></swag-tag-sl-i-bid>
 
-<template id=stsl-field-editor>
-<sl-button type="primary">Primary</sl-button>
+<template id=stsl-general-field-editor>
+<sl-textarea label={{name}}>{{val}}</sl-textarea>
 </template>
 <c-c copy from-prev-sibling></c-c>
 `;
@@ -22,3 +24,26 @@ export class SwagTagSL extends SwagTagBase {
 }
 SwagTagSL.is = 'swag-tag-sl';
 xc.define(SwagTagSL);
+export class SwagTagSLIBid extends IBid {
+    constructor() {
+        super(...arguments);
+        this.map = (x, idx) => {
+            if (x.type === undefined || x.type.text === undefined) {
+                return {
+                    ...x,
+                    localName: 'stsl-general-field-editor',
+                };
+            }
+            switch (x.type.text) {
+                case 'string':
+                case 'string | undefined':
+                    return {
+                        ...x,
+                        localName: 'stsl-general-field-editor'
+                    };
+            }
+        };
+    }
+}
+SwagTagSLIBid.is = 'swag-tag-sl-i-bid';
+xc.define(SwagTagSLIBid);
--- swag-tag-sl.ts
+++ swag-tag-sl.ts
@@ -1,18 +1,23 @@
 import {SwagTagBase, xc, xp, html} from 'swag-tag/lib/swag-tag-base.js';
-import('lib-id/li-bid.js');
+import {EnhancedClassField} from 'wc-info/types.d.js';
+import {IBid} from 'ib-id/i-bid.js';
+import {ClassField} from 'node_modules/custom-elements-manifest/schema.d.js';
 import('@shoelace-style/shoelace/dist/components/button/button.js');
+import('@shoelace-style/shoelace/dist/components/input/input.js');
+import('@shoelace-style/shoelace/dist/components/textarea/textarea.js');
 import('carbon-copy/c-c.js');
 import('pass-prop/p-p.js');
 import('if-diff/if-diff.js');
 
 
+
 //import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
 export const editor = html `
 <p-d observe=wc-info-fetch vft=fields to=[-list] m=1></p-d>
-<i-bid -list><stsl-field-editor></stsl-field-editor></i-bid>
+<swag-tag-sl-i-bid -list><stsl-general-field-editor></stsl-general-field-editor></swag-tag-sl-i-bid>
 
-<template id=stsl-field-editor>
-<sl-button type="primary">Primary</sl-button>
+<template id=stsl-general-field-editor>
+<sl-textarea label={{name}}>{{val}}</sl-textarea>
 </template>
 <c-c copy from-prev-sibling></c-c>
 `;
@@ -21,3 +26,24 @@ export class SwagTagSL extends SwagTagBase{
   editor = editor;
 }
 xc.define(SwagTagSL);
+
+export class SwagTagSLIBid extends IBid<EnhancedClassField>{
+  static is = 'swag-tag-sl-i-bid';
+   map = (x: EnhancedClassField, idx: number) => {
+    if(x.type === undefined || x.type.text === undefined){
+      return {
+        ...x,
+        localName: 'stsl-general-field-editor',
+      };
+    }
+    switch(x.type.text){
+      case 'string':
+      case 'string | undefined':
+        return {
+          ...x,
+          localName: 'stsl-general-field-editor'
+        };
+    }
+   }
+}
+xc.define(SwagTagSLIBid)
