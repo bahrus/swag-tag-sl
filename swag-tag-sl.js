98e024e555533098cd4ef533355a0119039acdfb
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-07-27 20:17:31 -0400

    ...

--- swag-tag-sl.js
+++ swag-tag-sl.js
@@ -8,12 +8,19 @@ import('pass-prop/p-p.js');
 import('if-diff/if-diff.js');
 //import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
 export const editor = html `
+<style>
+  [data-is-even="true"] *{
+    background-color: var(--even-row-bg-color, #f8f8f8);
+  }
+</style>
 <p-d observe=wc-info-fetch vft=fields to=[-list] m=1></p-d>
-<swag-tag-sl-i-bid -list><stsl-general-field-editor></stsl-general-field-editor></swag-tag-sl-i-bid>
-
+<swag-tag-sl-i-bid -list ><stsl-general-field-editor></stsl-general-field-editor></swag-tag-sl-i-bid>
+<div>iah</div>
 <template id=stsl-general-field-editor>
-<sl-textarea label="Property: {{name}}" help-text={{description}}>{{val}}</sl-textarea>
+<sl-textarea class=field-editor label="Property: {{name}}" help-text={{description}} data-name={{name}}>{{val}}</sl-textarea>
+<p-d on=sl-change vft from=stsl-general-field-editor to=div prop-from-target=dataset.name></p-d>
 <hr>
+<p>iah</p>
 </template>
 <c-c copy from-prev-sibling noshadow string-props='["name", "description"]' obj-props='["val"]'></c-c>
 `;
@@ -29,10 +36,17 @@ export class SwagTagSLIBid extends IBid {
     constructor() {
         super(...arguments);
         this.map = (x, idx) => {
+            const ds = {
+                dataset: {
+                    isEven: idx % 2 === 0,
+                }
+            };
             if (x.type === undefined || x.type.text === undefined) {
                 return {
                     ...x,
                     localName: 'stsl-general-field-editor',
+                    ...ds,
+                    //attributes: {even: idx % 2 === 0}
                 };
             }
             switch (x.type.text) {
@@ -40,11 +54,13 @@ export class SwagTagSLIBid extends IBid {
                 case 'string | undefined':
                     return {
                         ...x,
+                        ...ds,
                         localName: 'stsl-general-field-editor'
                     };
                 default:
                     return {
                         ...x,
+                        ...ds,
                         localName: 'stsl-general-field-editor'
                     };
             }
--- swag-tag-sl.ts
+++ swag-tag-sl.ts
@@ -13,12 +13,19 @@ import('if-diff/if-diff.js');
 
 //import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
 export const editor = html `
+<style>
+  [data-is-even="true"] *{
+    background-color: var(--even-row-bg-color, #f8f8f8);
+  }
+</style>
 <p-d observe=wc-info-fetch vft=fields to=[-list] m=1></p-d>
-<swag-tag-sl-i-bid -list><stsl-general-field-editor></stsl-general-field-editor></swag-tag-sl-i-bid>
-
+<swag-tag-sl-i-bid -list ><stsl-general-field-editor></stsl-general-field-editor></swag-tag-sl-i-bid>
+<div>iah</div>
 <template id=stsl-general-field-editor>
-<sl-textarea label="Property: {{name}}" help-text={{description}}>{{val}}</sl-textarea>
+<sl-textarea class=field-editor label="Property: {{name}}" help-text={{description}} data-name={{name}}>{{val}}</sl-textarea>
+<p-d on=sl-change vft from=stsl-general-field-editor to=div prop-from-target=dataset.name></p-d>
 <hr>
+<p>iah</p>
 </template>
 <c-c copy from-prev-sibling noshadow string-props='["name", "description"]' obj-props='["val"]'></c-c>
 `;
@@ -31,10 +38,17 @@ xc.define(SwagTagSL);
 export class SwagTagSLIBid extends IBid<EnhancedClassField>{
   static is = 'swag-tag-sl-i-bid';
    map = (x: EnhancedClassField, idx: number) => {
+     const ds = {
+       dataset: {
+        isEven: idx % 2 === 0,
+       }
+      };
     if(x.type === undefined || x.type.text === undefined){
       return {
         ...x,
         localName: 'stsl-general-field-editor',
+        ...ds,
+        //attributes: {even: idx % 2 === 0}
       };
     }
     switch(x.type.text){
@@ -42,11 +56,13 @@ export class SwagTagSLIBid extends IBid<EnhancedClassField>{
       case 'string | undefined':
         return {
           ...x,
+          ...ds,
           localName: 'stsl-general-field-editor'
         };
       default:
         return {
           ...x,
+          ...ds,
           localName: 'stsl-general-field-editor'
         };
     }
