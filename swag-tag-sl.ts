c5c07a7b95c0ba9173003b56faf18556dbf078a8
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-07-31 00:25:58 -0400

    ...

--- swag-tag-sl.ts
+++ swag-tag-sl.ts
@@ -5,7 +5,7 @@ import {ClassField} from 'node_modules/custom-elements-manifest/schema.d.js';
 import('@shoelace-style/shoelace/dist/components/button/button.js');
 import('@shoelace-style/shoelace/dist/components/input/input.js');
 import('@shoelace-style/shoelace/dist/components/textarea/textarea.js');
-import('carbon-copy/c-c.js');
+import('d-fine/d-fine.js');
 import('pass-prop/p-p.js');
 import('if-diff/if-diff.js');
 
@@ -19,15 +19,16 @@ export const editor = html `
   }
 </style>
 <p-d observe=wc-info-fetch vft=fields to=[-list] m=1></p-d>
-<swag-tag-sl-i-bid -list ><stsl-general-field-editor></stsl-general-field-editor></swag-tag-sl-i-bid>
+<swag-tag-sl-i-bid -list tag=stsl-general-field-editor></swag-tag-sl-i-bid>
 <div>iah</div>
-<template id=stsl-general-field-editor>
-<sl-textarea class=field-editor label="Property: {{name}}" help-text={{description}} data-name={{name}}>{{val}}</sl-textarea>
-<p-d on=sl-change vft from=stsl-general-field-editor to=div prop-from-target=dataset.name></p-d>
+
+<template>
+<sl-textarea class=field-editor label="Property: {{name}}" help-text={{description}}>{{val}}</sl-textarea>
+<p-d on=sl-change vft from=stsl-general-field-editor to=div prop={{name}}></p-d>
 <hr>
 <p>iah</p>
 </template>
-<c-c copy from-prev-sibling noshadow string-props='["name", "description"]' obj-props='["val"]'></c-c>
+<d-fine fps as=stsl-general-field-editor noshadow sp='["name", "description"]' op='["val"]'></d-fine>
 `;
 export class SwagTagSL extends SwagTagBase{
   static is = 'swag-tag-sl';
